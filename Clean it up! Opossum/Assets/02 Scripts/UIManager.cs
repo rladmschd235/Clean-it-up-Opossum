4d12f317b27920f5b3d4f497084c9fa34d6d76b3
euna <dmsdk623@gmail.com>
2024-05-20 11:50:48 +0900

    UIManger 기본 세팅 / set

--- Clean it up! Opossum/Assets/02 Scripts/UIManager.cs
+++ Clean it up! Opossum/Assets/02 Scripts/UIManager.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject inGameUI;
+    public GameObject mainUI;
+    public GameObject overUI;
+    public GameObject clearUI;
+
+    public Button reButton;
+    public Button settingButton;
+    public Button pauseButton;
+
     void Start()
     {
-        
+        HideAllUI();
+
+        ShowMainUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    void HideAllUI() // 모든 UI 숨김
     {
-        
+        inGameUI.SetActive(false);
+        mainUI.SetActive(false);
+        overUI.SetActive(false);
+        clearUI.SetActive(false);
+
+        reButton.gameObject.SetActive(false);
+        settingButton.gameObject.SetActive(false);
+        pauseButton.gameObject.SetActive(false);
+    }
+
+    public void ShowInGameUI() // 인게임 UI 패널 활성화
+    {
+        HideAllUI();
+        inGameUI.SetActive(true);
+
+        reButton.gameObject.SetActive(false);
+        settingButton.gameObject.SetActive(true);
+        pauseButton.gameObject.SetActive(true);
+    }
+
+    public void ShowMainUI() // 메인 화면 UI 패널 활성화
+    {
+        HideAllUI();
+        mainUI.SetActive(true);
+
+        reButton.gameObject.SetActive(true);
+        settingButton.gameObject.SetActive(true);
+        pauseButton.gameObject.SetActive(true);
+    }
+
+    public void ShowOverUI() // 게임 오버 UI 패널 활성화
+    {
+        HideAllUI();
+        overUI.SetActive(true);
+
+        reButton.gameObject.SetActive(true);
+        settingButton.gameObject.SetActive(false);
+        pauseButton.gameObject.SetActive(false);
+    }
+    public void ShowClearUI() // 게임 클리어 UI 패널 활성화
+    {
+        HideAllUI();
+        clearUI.SetActive(true);
+
+        reButton.gameObject.SetActive(true);
+        settingButton.gameObject.SetActive(false);
+        pauseButton.gameObject.SetActive(false);
     }
 }
