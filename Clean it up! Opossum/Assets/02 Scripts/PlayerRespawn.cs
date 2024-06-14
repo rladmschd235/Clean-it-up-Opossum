dcc4282a16df5f9c1d06b7dc7aa674a4e58f40a4
dongjin0457 <pianistdj@naver.com>
2024-06-14 11:15:23 +0900

    PlayerRespawn 수정
    - 바닥위에서 벗어날 경우 조건식 추가

--- Clean it up! Opossum/Assets/02 Scripts/PlayerRespawn.cs
+++ Clean it up! Opossum/Assets/02 Scripts/PlayerRespawn.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
 {
+    public Transform floor;
+
     private Vector3 startPos;
+
+    private void Start()
+    {
+        floor = GameObject.FindWithTag("Area").transform;
+    }
 
     private void Update()
     {
@@ -13,8 +20,20 @@ public class PlayerRespawn : MonoBehaviour
 
     private void DeathCheck()
     {
-        if (GameScenes.globalPlayerDrag.dragCnt == GameScenes.globalStageManager.chance && 
-            GameScenes.globalStageManager.clearCount > GameScenes.globalStageManager.trashCount)
+        Vector3 floorPosition = floor.transform.position;
+        Vector3 floorScale = floor.transform.localScale;
+
+        // 바닥의 크기를 고려하여 x, z 범위를 계산합니다.
+        float floorMinX = floorPosition.x - floorScale.x / 2;
+        float floorMaxX = floorPosition.x + floorScale.x / 2;
+        float floorMinZ = floorPosition.z - floorScale.z / 2;
+        float floorMaxZ = floorPosition.z + floorScale.z / 2;
+
+        //드래그 카운트 한계를 초과하면서 쓰레기 갯수를 다 채우지 못한 경우 혹은 맵밖으로 벗어난 경우
+        if ((GameScenes.globalPlayerDrag.dragCnt == GameScenes.globalStageManager.chance && 
+            GameScenes.globalStageManager.clearCount > GameScenes.globalStageManager.trashCount) || 
+            (transform.position.x < floorMinX || transform.position.x  > floorMaxX || 
+            transform.position.z < floorMinZ || transform.position.z > floorMaxZ))
         {
             GameScenes.globalGameManager.GameOver();
             PosLoad();
